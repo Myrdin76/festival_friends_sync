21f43d26c570501cbbbca4e2dedb22eb5ac517a8
Myrdin van der Horst <m.vanderhorst@districon.com>
2023-07-27 23:46:54 +0200

    checkboxes

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
-from flask import render_template
+from flask import render_template, request
 import pandas as pd
 
 from app import app
 
-data = pd.read_csv('app/lowlands_schema.csv').to_dict('records')
+data = pd.read_csv('app/lowlands_schema.csv')
+data['id'] = data.index
+stages = data['stage'].unique().tolist()
 
 @app.route('/index')
 def index():
@@ -12,10 +14,24 @@ def index():
 
 @app.route('/')
 def timetable():
-    return render_template('timetable.html')
+    return render_template('timetable.html', stages=stages)
 
 
 @app.route('/fill_table')
 def fill_table():
-    
-    return render_template('fill_table.html', data=data)
+    global data
+    stage = request.args.get('stageselector')
+    print("SELECTED STAGE: ", stage)
+    if stage:
+        ndata = data[data['stage'] == stage].to_dict('records')
+    else:
+        ndata = data.to_dict('records')
+        
+    return render_template('fill_table.html', data=ndata)
+
+@app.get('/select_artist')
+def select_artist():
+    res = request.args
+    print(res)
+
+    return ""
